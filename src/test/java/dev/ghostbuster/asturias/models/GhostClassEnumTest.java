baf3da51786ed488f6a732f2b0810aa23511602f
Lgtf5 <lgtablasf5@gmail.com>
2025-02-15 15:50:44 +0100

    testForValiumsPropertiesVerificated for the GhostClassEnumtest succesfull

--- src/test/java/dev/ghostbuster/asturias/models/GhostClassEnumTest.java
+++ src/test/java/dev/ghostbuster/asturias/models/GhostClassEnumTest.java
@@ -1,6 +1,9 @@
 package dev.ghostbuster.asturias.models;
+import dev.ghostbuster.asturias.models.GhostClassEnum;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,8 +13,26 @@ public class GhostClassEnumTest {
     @DisplayName ("se testa todos los valores que tienen las clases y sus propiedades")
     public void testForValiumsPropertiesVerificated() {
         assertAll(
-            () -> assertThat(CLASE_I.getDescriptionClass(), is("Manifestación menor")),
-            () -> assertThat(CLASE_I.getLevelClass(), is(1)),
+            () -> assertThat(GhostClassEnum.CLASE_I.getDescriptionClass(), is("Manifestación menor")),
+            () -> assertThat(GhostClassEnum.CLASE_I.getLevelClass(), is(1)),
+
+            () -> assertThat(GhostClassEnum.CLASE_II.getDescriptionClass(), is("Aparición móvil")),
+            () -> assertThat(GhostClassEnum.CLASE_II.getLevelClass(), is(2)),
+
+            () -> assertThat(GhostClassEnum.CLASE_III.getDescriptionClass(), is("Entidad inteligente")),
+            () -> assertThat(GhostClassEnum.CLASE_III.getLevelClass(), is(3)),
+
+            () -> assertThat(GhostClassEnum.CLASE_IV.getDescriptionClass(), is("Fantasma histórico")),
+            () -> assertThat(GhostClassEnum.CLASE_IV.getLevelClass(), is(4)),
+
+            () -> assertThat(GhostClassEnum.CLASE_V.getDescriptionClass(), is("Espíritu antropomorfo")),
+            () -> assertThat(GhostClassEnum.CLASE_V.getLevelClass(), is(5)),
+            
+            () -> assertThat(GhostClassEnum.CLASE_VI.getDescriptionClass(), is("Espíritu demoníaco")),
+            () -> assertThat(GhostClassEnum.CLASE_VI.getLevelClass(), is(6)),
+
+            () -> assertThat(GhostClassEnum.CLASE_VII.getDescriptionClass(), is("Entidad ultraterrena")),
+            () -> assertThat(GhostClassEnum.CLASE_VII.getLevelClass(), is(7))
         );
 
     }
