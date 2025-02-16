cdb86ed026bf87b18db7e3991f06c42195cfc913
Lgtf5 <lgtablasf5@gmail.com>
2025-02-17 00:57:13 +0100

    Revert "GhostDangerEnum refactorized"
    This reverts commit 6b11057b2d1314f5bdd89cc0b02fdbc2e759ffd2.

--- src/main/java/dev/ghostbuster/asturias/models/GhostDangerLevelEnum.java
+++ src/main/java/dev/ghostbuster/asturias/models/GhostDangerLevelEnum.java
@@ -16,30 +16,20 @@ public enum GhostDangerLevelEnum {
     }
 
     public String getLevelName() {
-        switch (this) {
-            case BAJO: return "Bajo";
-            case MEDIO: return "Medio";
-            case ALTO: return "Alto";
-            case CRITICO: return "Crítico";
-            default: return name();
-        }
+        return levelName;
     }
+
     public int getLevelDanger() {
         return levelDanger;
     }
     public static String[] getDisplayNames() {
-        return java.util.Arrays.stream(values())
-            .map(level -> {
-                switch (level) {
-                    case BAJO: return "Bajo";
-                    case MEDIO: return "Medio";
-                    case ALTO: return "Alto";
-                    case CRITICO: return "Crítico";
-                    default: return level.name();
-                }
-            })
-            .toArray(String[]::new);
-    }
+        GhostDangerLevelEnum[] values = GhostDangerLevelEnum.values();
+        String[] displayNames = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            displayNames[i] = values[i].getLevelName(); // Usa getLevelName() para obtener el nombre descriptivo
+        }
+        return displayNames;
+} 
 }
 
 
