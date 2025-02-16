b8079b19096d03ee488b73e94e0378c0d35355e3
Lgtf5 <lgtablasf5@gmail.com>
2025-02-17 00:35:51 +0100

    testDisplayDangerLevel succesfull

--- src/main/java/dev/ghostbuster/asturias/models/GhostDangerLevelEnum.java
+++ src/main/java/dev/ghostbuster/asturias/models/GhostDangerLevelEnum.java
@@ -2,12 +2,32 @@ package dev.ghostbuster.asturias.models;
 
 public enum GhostDangerLevelEnum {
 
+    BAJO("Bajo", 1),
+    MEDIO("Medio", 2),
+    ALTO("Alto", 3),
+    CRITICO("Crítico", 4);
 
+    private final String displayName;
+    private final int level;
+
+    GhostDangerLevelEnum(String displayName, int level) {
+        this.displayName = displayName;
+        this.level = level;
     }
-    
 
 
 
-
-
-
+    public static String[] getDisplayNames() {
+        return java.util.Arrays.stream(values())
+            .map(level -> {
+                switch (level) {
+                    case BAJO: return "Bajo";
+                    case MEDIO: return "Medio";
+                    case ALTO: return "Alto";
+                    case CRITICO: return "Crítico";
+                    default: return level.name();
+                }
+            })
+            .toArray(String[]::new);
+    }
+}
--- src/test/java/dev/ghostbuster/asturias/models/GhostDangerLevelEnumTest.java
+++ src/test/java/dev/ghostbuster/asturias/models/GhostDangerLevelEnumTest.java
@@ -7,26 +7,24 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
-
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GhostDangerLevelEnumTest {
-   
+
+    
+
     @Test
     @DisplayName ("test a display names of danger levels")
     void testDisplayDangerLevel() {
 
         String[] displayNames = GhostDangerLevelEnum.getDisplayNames();
 
-        assertThat(displayNames, hasItem ("Bajo"));
-        assertThat(displayNames, hasItem ("Medio"));
-        assertThat(displayNames, hasItem ("Alto"));
-        assertThat(displayNames, hasItem ("Crítico"));
+        MatcherAssert.assertThat(displayNames.length, Matchers.is(4));
 
-        assertThat(displayNames.length, equalTo(4));
-
+        MatcherAssert.assertThat(displayNames[0], Matchers.is("Bajo"));
+        MatcherAssert.assertThat(displayNames[1], Matchers.is("Medio"));
+        MatcherAssert.assertThat(displayNames[2], Matchers.is("Alto"));
+        MatcherAssert.assertThat(displayNames[3], Matchers.is("Crítico"));
     }
 
-
-}   
+}
